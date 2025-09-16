00e7a7fd12f478a3a615264d289511c373b1fa13
Любимов Кирилл Алексеевич <llyryk934@gmail.com>
2025-09-16 23:30:35 +0700

    add client, server on python

---
+++ examples/server.py
@@ -0,0 +1,18 @@
+import socket
+
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(1)
+
+conn, addr = sock.accept()
+
+print('Connected to ', addr)
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data + b" GOIDA!")
+    print(data)
+
+conn.close()
